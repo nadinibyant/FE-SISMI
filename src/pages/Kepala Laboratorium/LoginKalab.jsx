5ba640da1aaff472f997137b731ccf94150e5286
Nadini Annisa Byant <89978510+nadinibyant@users.noreply.github.com>
2024-06-23 23:40:19 +0700

    Update LoginKalab.jsx

--- src/pages/Kepala Laboratorium/LoginKalab.jsx
+++ src/pages/Kepala Laboratorium/LoginKalab.jsx
@@ -2,7 +2,7 @@ import FormLoginKalab from "../../components/form/FormLoginKalab"
 export default function LoginKalab (){
     return (
         <>
-        <FormLoginKalab label1={'nip'} label2={'password'} gambar={'/public/logo-kalab.svg'}/>
+        <FormLoginKalab label1={'nip'} label2={'password'} gambar={'/logo-kalab.svg'}/>
         </>
     )
 }
