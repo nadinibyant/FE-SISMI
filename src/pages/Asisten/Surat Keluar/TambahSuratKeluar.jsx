a9c1c45c90b946e2207b2d366e129b5982e06bf2
Nadini Annisa Byant <89978510+nadinibyant@users.noreply.github.com>
2024-06-24 13:46:54 +0700

    Update TambahSuratKeluar.jsx

--- src/pages/Asisten/Surat Keluar/TambahSuratKeluar.jsx
+++ src/pages/Asisten/Surat Keluar/TambahSuratKeluar.jsx
@@ -55,7 +55,7 @@ export default function TambahSuratKeluar (){
 
     return (
         <>
-        <SidebarAsisten profile={'/asisten/profile'} nama_asisten={'Nadini Annisa Byant'}>
+        <SidebarAsisten profile={'/asisten/profile'} nama_asisten={'Asisten'}>
         <div className="p-10 ps-12">
                 <form onSubmit={handleSubmit}>
                     <p className="text-primary font-bold text-3xl">Tambah Surat Keluar</p>
