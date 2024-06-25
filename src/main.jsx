92a9df2c61b0221b35abd887cf14ed4fa41747dd
Nadini Annisa Byant <89978510+nadinibyant@users.noreply.github.com>
2024-06-25 22:32:05 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+      for (let registration of registrations) {
+          registration.unregister();
+      }
+  });
+}
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
